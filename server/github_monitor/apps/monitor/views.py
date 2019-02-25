fa7ba9dcc89552c394c196429e7fc9ed13ca55e3
BlackFrame <wyw_r1@163.com>
2019-02-25 22:41:58 +0800

    updated

--- server/github_monitor/apps/monitor/views.py
+++ server/github_monitor/apps/monitor/views.py
@@ -10,6 +10,7 @@ from .models.task import Task
 from .serializers.leakage_serializers import LeakageSerializer
 from .serializers.token_serializers import TokenSerializer
 from .serializers.task_serializers import TaskSerializer
+from django.db.models import Q
 
 
 class LeakageViewSet(ModelViewSet):
@@ -52,7 +53,7 @@ class TaskViewSet(ModelViewSet):
 
     @action(methods=['PUT'], detail=True, url_path='flush_task', url_name='flush_task')
     def flush_task(self, request, pk):
-        Leakage.objects.filter(task_id=pk).filter(status != 1).update(status=2)
+        Leakage.objects.filter(task_id=pk).filter(~Q(status=1)).update(status=2)
         return Response('仓库组加白成功', status=status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
